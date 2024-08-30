23cc5bee95c9c1a03553bc838b5d862b699bebe3
BenSymons <ben.symons@bbc.co.uk>
2024-08-30 15:44:42 +0100

    Revert "feat: add the toPower function"
    This reverts commit 9f3d0a33b519a838d487e2884fc195464c2fcf9d.

--- index.js
+++ index.js
@@ -18,8 +18,4 @@ function modulo (a, b) {
   a % b;
 };
 
-function toPower (a, b) {
-  a ** b;
-};
-
-module.exports = { add, subtract, multiply, divide, modulo, toPower };
+module.exports = { add, subtract, multiply, divide, modulo };
