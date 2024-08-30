2817ae2ce25003e43d9ba3dde77e4a734b57617d
BenSymons <ben.symons@bbc.co.uk>
2024-08-30 08:59:17 +0100

    feat: add subtract function

--- index.test.js
+++ index.test.js
@@ -1,7 +1,11 @@
-const { add } = require('.')
+const { add, subtract } = require('.')
 
 describe('Unit tests', () => {
   test('add function adds two numbers', () => {
     expect(add(4, 5)).toBe(9)
+  })
+
+  test('subtract function subtracts two numbers', () => {
+    expect(subtract(10, 2)).toBe(8);
   })
 })
