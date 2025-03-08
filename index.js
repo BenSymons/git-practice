943742f302043806a54b5cf58065541142e69c09
Ben Symons <bsymonsb@hotmail.co.uk>
2025-03-08 10:46:06 +0000

    change the name of the functions

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const add = (a, b) => a + b;
 
-const subtract = (a, b) => a - b;
+const minus = (a, b) => a - b;
 
-const multiply = (a, b) => a * b;
+const times = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
 
